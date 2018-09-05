c1c63a3aa87673ec11e7d6e4dbd7a0864af67f0f
Downie <downie4@basswood.llnl.gov>
2018-09-05 16:44:13 -0700

    Restore panel state

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,103 @@
-import {JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
-import {ICommandPalette} from '@jupyterlab/apputils';
-import {Widget} from '@phosphor/widgets';
+import { JupyterLab, JupyterLabPlugin, ILayoutRestorer } from '@jupyterlab/application';
+import { ICommandPalette, InstanceTracker } from '@jupyterlab/apputils';
+import { JSONExt } from '@phosphor/coreutils'
+import { Widget } from '@phosphor/widgets';
+import { Message } from '@phosphor/messaging';
 import '../style/index.css';
 
+/**
+ * An xckd comic viewer
+ */
+class XkcdWidget extends Widget {
+  /**
+   * Construct a new xkcd widget.
+   */
+  constructor() {
+    super();
+
+    this.id = 'jupyter-react-ext';
+    this.title.label = 'xkcd.com';
+    this.title.closable = true;
+    this.addClass('jp-xkcdWidget');
+
+    this.img = document.createElement('img');
+    this.img.className = 'jp-xkcdCartoon';
+    this.node.appendChild(this.img);
+
+    this.img.insertAdjacentHTML('afterend',
+      `<div class="jp-xkcdAttribution">
+        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+        </a>
+      </div>`
+    );
+  }
+
+  /**
+   * The image element associated with the widget.
+   */
+  readonly img: HTMLImageElement;
+
+  /**
+   * Handle update requests for the widget.
+   */
+  onUpdateRequest(msg: Message): void {
+    fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+      return response.json();
+    }).then(data => {
+      this.img.src = data.img;
+      this.img.alt = data.title;
+      this.img.title = data.alt;
+    });
+  }
+};
+
+/**
+ * Activate the xckd widget extension.
+ */
+function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRestorer) {
+  console.log('JupyterLab extension jupyter-react-ext is activated!');
+
+  // Declare a widget variable
+  let widget: XkcdWidget;
+
+  // Add an application command
+  const command: string = 'xkcd:open';
+  app.commands.addCommand(command, {
+    label: 'Random xkcd comic',
+    execute: () => {
+      if (!widget) {
+        // Create a new widget if one does not exist
+        widget = new XkcdWidget();
+        widget.update();
+      }
+      if (!tracker.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker.add(widget);
+      }
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.addToMainArea(widget);
+      } else {
+        // Refresh the comic in the widget
+        widget.update();
+      }
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+
+  // Track and restore the widget state
+  let tracker = new InstanceTracker<Widget>({ namespace: 'xkcd' });
+  restorer.restore(tracker, {
+    command,
+    args: () => JSONExt.emptyObject,
+    name: () => 'xkcd'
+  });
+};
 
 /**
  * Initialization data for the jupyter-react-ext extension.
@@ -10,57 +105,8 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyter-react-ext',
   autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyter-react-ext is activated!');
-
-    // Create a single widget
-    let widget: Widget = new Widget();
-    widget.id = 'xkcd-jupyterlab';
-    widget.title.label = 'xkcd.com';
-    widget.title.closable = true;
-    widget.addClass('jp-xkcdWidget');
-
-    // Add an image element to the panel
-    let img = document.createElement('img');
-    img.className = 'jp-xkcdCartoon';
-    widget.node.appendChild(img);
-
-    // Add an attribution badge
-    img.insertAdjacentHTML('afterend',
-      `<div class="jp-xkcdAttribution">
-        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
-          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
-        </a>
-      </div>`
-    );
-
-    // Fetch info about a random comic
-    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-      return response.json();
-    }).then(data => {
-      img.src= data.img;
-      img.alt = data.title;
-      img.title = data.alt;
-    })
-
-    // Add an application command
-    const command: string = 'xkcd:open';
-    app.commands.addCommand(command, {
-      label: 'Random xkcd comic',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.addToMainArea(widget);
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    // Add the command to the palette.
-    palette.addItem({command, category: 'Tutorial'});
-  }
+  requires: [ICommandPalette, ILayoutRestorer],
+  activate: activate
 };
 
 export default extension;
