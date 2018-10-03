ada8e6f17e6ee8d560c75a6d541554245cf09c89
Downie <downie4@ClimatePml-bl.llnl.gov>
2018-10-02 17:11:41 -0700

    Updated left side bar buttons, modified README to have more install instructions

--- README.md
+++ README.md
@@ -10,17 +10,44 @@ Learning to make extensions for JupyterLab and incorporating React.
 ## Installation
 
 ```bash
-jupyter labextension install jupyter-react-ext
+    #Install
+    jupyter labextension install jupyter-react-ext
+
+    #To run
+    jupyter lab
 ```
 
-## Development
+## Development Install
 
 For a development install (requires npm version 4 or later), do the following in the repository directory:
 
 ```bash
-npm install
-npm run build
-jupyter labextension link .
+
+    #Create the environment
+    conda create -n jupyter-vcdat -c cdat/label/nightly -c conda-forge -c cdat ipython nodejs vcs jupyterlab jupyter flake8 autopep8 pip nb_conda jupyterhub ipywidgets python=3
+    source activate jupyter-vcdat
+
+    # Go to vcs repo
+    git clone https://github.com/CDAT/vcs.git
+    cd vcs
+    git checkout boxfill_widgets_jupyter
+    python setup.py install --old-and-unmanageable
+
+    # Install sidecar
+    pip install sidecar
+    jupyter labextension install @jupyter-widgets/jupyterlab-manager
+    jupyter labextension install @jupyter-widgets/jupyterlab-sidecar
+
+    #install the react-tutorial extension
+    cd ..
+    git clone https://github.com/downiec/jupyter-react-ext.git
+    cd jupyter-react-ext
+    git checkout vcdat2.0
+    jupyter labextension install
+
+    #To run, got to jupyter-react-ext repo
+    jupyter lab
+
 ```
 
 To rebuild the package and the JupyterLab app:
--- src/components/AddEditRemoveNav.tsx
+++ src/components/AddEditRemoveNav.tsx
@@ -5,7 +5,6 @@ class AddEditRemoveNav extends React.PureComponent <any> {
         
     constructor(props: any) {
         super(props);
-        this.props = props;
     }
 
     render() {
@@ -14,19 +13,22 @@ class AddEditRemoveNav extends React.PureComponent <any> {
                 <div className="container-fluid">
                     <div>
                         <ul className="nav navbar-nav navbar-right side-nav">
-                            <li className="action-add-button">
+                            {/*<li className="action-add-button">
                                 <a href='#' onClick={this.props.addAction} title={this.props.addText ? this.props.addText : ""}>
                                     <i className={`glyphicon glyphicon-plus-sign ${this.props.addAction ? "" : "disabled"}`}></i>
+                                    <i className={`glyphicon glyphicon-plus-sign disabled`}></i>
                                 </a>
-                            </li>
+                            </li>*/}
                             <li className="action-edit-button">
                                 <a href="#" onClick={this.props.editAction} title={this.props.editText ? this.props.editText : ""}>
-                                    <i className={`glyphicon glyphicon-edit ${this.props.editAction ? "" : "disabled"}`}></i>
+                                    {/*<i className={`glyphicon glyphicon-edit ${this.props.editAction ? "" : "disabled"}`}></i>*/}
+                                    <i className={`glyphicon glyphicon-edit disabled`}></i>
                                 </a>
                             </li>
                             <li className="action-remove-button">
                                 <a href="#" onClick={this.props.removeAction} title={this.props.removeText ? this.props.removeText : ""}>
-                                    <i className={`glyphicon glyphicon-remove-sign ${this.props.removeAction ? "" : "disabled"}`}></i>
+                                    {/*<i className={`glyphicon glyphicon-remove-sign ${this.props.removeAction ? "" : "disabled"}`}></i>*/}
+                                    <i className={`glyphicon glyphicon-remove-sign disabled`}></i>
                                 </a>
                             </li>
                         </ul>
