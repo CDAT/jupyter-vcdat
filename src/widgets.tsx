15b04aeb43441b728d205e6b55f5c74dc90deeeb
Downie <downie4@ClimatePml-bl.llnl.gov>
2018-10-05 23:11:23 -0700

    Finished variable, templates and graphicMethods populating lists from console output. Updated injection code, widget refresh button still has bugs.

--- src/widgets.tsx
+++ src/widgets.tsx
@@ -5,26 +5,16 @@ import { LeftSideBar } from './components/LeftSideBar';
 import { CommandRegistry } from '@phosphor/commands';
 import { DocumentRegistry } from '@jupyterlab/docregistry';
 import { ConsolePanel } from '@jupyterlab/console';
-import { Cell } from '@jupyterlab/cells';
 
-/*
-//Experimental
-function varCapture(){
 
-	commands.execute('console:inject', {
-		activate: true,
-	}).then(consolePanel => {
-		consolePanel.session.ready.then(() => {
-			consolePanel.console.inject(getVarCmd);
-		});
-	});
-	console.log("Injected var command.");
-	commands.execute('console:clear');
-	console.log("Cleared console");
-	
-};*/
+/*Test python script
+import cdms2
+import vcs
+data = cdms2.open('clt.nc')
+clt = data('clt')
+*/
 
-/*
+/*Refresh python script
 import __main__
 import json
 import cdms2
@@ -59,33 +49,40 @@ print(graphic_methods()["boxfill"])
 print(list_all())
 */
 
-const getVarCmd = 
-"import cdms2\n\
-import __main__\n\
+const GET_VARS_CMD = 
+'import __main__\n\
 import json\n\
-def variables_generator():\n\
-\tfor nm, obj in __main__.__dict__.items():\n\
-\t\tif isinstance(obj, cmds2.MV2.TransientVariable):\n\
-\t\t\tyield obj.id\n\
-\treturn\n\
+import cdms2\n\
+import vcs\n\
 def variables():\n\
 \tout = []\n\
 \tfor nm, obj in __main__.__dict__.items():\n\
 \t\tif isinstance(obj, cdms2.MV2.TransientVariable):\n\
-\t\t\tout.append(obj.id)\n\
+\t\t\tout+=[nm]\n\
 \treturn out\n\
-with open('tmp.txt','w') as filehandle:\n\
-\tjson.dump(variables(),filehandle)\nprint(variables())";
+def graphic_methods():\n\
+\tout = {}\n\
+\tfor typ in vcs.graphicsmethodlist():\n\
+\t\tout[typ] = vcs.listelements(typ)\n\
+\treturn out\n\
+def templates():\n\
+\treturn vcs.listelements("template")\n\
+def list_all():\n\
+\tout = {}\n\
+\tout["variables"] = variables()\n\
+\tout["gm"] = graphic_methods()\n\
+\tout["template"] = templates()\n\
+\treturn out\n\
+print("{}|{}|{})".format(variables(),templates(),graphic_methods()))';
 
 export class LeftSideBarWidget extends Widget {
 
-    div: HTMLDivElement;
+    div: HTMLDivElement; //The div container for this widget
     commands: CommandRegistry;
     context: DocumentRegistry.Context;
     currentPanel: ConsolePanel;
     variables: string;
-    props: any;
-    cell: Cell;
+    props: any; //An object that stores the props to pass down to LeftSideBar
 
     constructor(commands: CommandRegistry, context: DocumentRegistry.Context){
         super();
@@ -101,6 +98,8 @@ export class LeftSideBarWidget extends Widget {
             variables: ["TestVar1","TestVar2","TestVariable3"],
             graphicsMethods: {"GraphicType1":['method a','method b','method c'],"GraphicType2":["method a","method z"],"GraphicType3":["method 1","method 2","method 3"]},
             templates: ["Template1","Template2","Template3"],
+
+            //The handlers below are run when a variable, template or graphicMethod link is clicked
             varClickHandler: (listName: string, varName: string)=>{
                 console.log("Variable: " + varName + " was clicked.");
             },
@@ -110,18 +109,56 @@ export class LeftSideBarWidget extends Widget {
             graphClickHandler: (graphicType: string, graphicName: string)=>{
                 console.log("GraphicType: " + graphicType + " method: '" + graphicName + "' was clicked.");
             },
+            //These are the refresh and plot handlers.
             refreshAction: () => {
                 this.updateVars();
             },
             plotAction: () => {
+                //Need to do some plot injection code here
                 this.updateVars();
             }
         };
         ReactDOM.render(<LeftSideBar {...this.props}></LeftSideBar>,this.div);
 
         this.updateVars = this.updateVars.bind(this);
-
+        this.reRender = this.reRender.bind(this);
         this.updateVars();
+    }
+
+    //Called whenever the LeftSideBar needs it's props updated.
+    reRender(){
+        ReactDOM.unmountComponentAtNode(this.div);//Remove old LeftSideBar (avoid memory leak)
+        ReactDOM.render(<LeftSideBar {...this.props}></LeftSideBar>,this.div);
+    }
+
+    //Helper function to convert console output string to an object/dictionary
+    outputStrToDict(output: string){
+        var dict: any = {variables: {},templates:{},graphicsMethods:{}};
+        var outputElements = output.replace(/\'/g,"").split('|');
+        dict.variables = outputElements[0].slice(1,-1).split(',');
+        dict.templates = outputElements[1].slice(1,-1).split(',');
+        
+        //var pattern = /?=\]\}\)/;
+        var first: boolean = true;
+        var prevKey: string = "";
+        outputElements[2].slice(1,-1).split(':').forEach((str)=>{
+            if(first){//first element is only a key
+                dict.graphicsMethods[str] = [];
+                prevKey = str;
+                first = false;
+            }
+            else if(str.endsWith("]})")){//last element is only a value
+                dict.graphicsMethods[prevKey] = str.slice(2,-3).split(',');
+            }
+            else{//A value/key pair
+                var pair = str.split('], ');
+                console.log(pair);
+                dict.graphicsMethods[prevKey] = pair[0].slice(2).split(',');
+                prevKey = pair[1];
+            }
+        });
+
+        return dict;
     }
 
     //This is where the code injection occurs in the current console.
@@ -137,31 +174,62 @@ export class LeftSideBarWidget extends Widget {
             }).then(consolePanel => {
                 consolePanel.session.ready.then(() => {
                     this.currentPanel = consolePanel;
+
                     //Temp cmd for testing
                     var injectCmd = "import cdms2\nimport vcs\ndata = cdms2.open(\'" +
-                    this.context.session.path + "\')\nclt = data('clt')";
+                    this.context.session.path + "\')\nclt = data('clt')\ntestVar = 5\nclt3 = clt*3";
                     this.currentPanel.console.inject(injectCmd);
 
                     //Command to put variables
-                    this.currentPanel.console.inject(getVarCmd).then((consolePanel)=>{
+                    this.currentPanel.console.inject(GET_VARS_CMD).then((consolePanel)=>{
 
-                        //Once injection is done read output
-                        console.log("Output elements:");
-                        var outputElements: string = document.getElementsByClassName("jp-OutputArea-output")[0].getElementsByTagName("pre")[0].innerHTML;
-                        console.log(outputElements);
+                        //Once injection is done read output as string
+                        var consoleOutputStr: string = document.getElementsByClassName("jp-OutputArea-output")[0].getElementsByTagName("pre")[0].innerHTML;
+                        var outputObj = this.outputStrToDict(consoleOutputStr);
+
+                        //Update props
+                        this.props.variables = outputObj["variables"];
+                        this.props.templates = outputObj["templates"];
+                        this.props.graphicsMethods = outputObj["graphicsMethods"];
+
+                        //Rerender the LeftSideBar
+                        this.reRender();
+
+                        //Calc the number of cells in console and remove last cell once props are updated
+                        var cellCount = this.currentPanel.console.cells.length;
+                        console.log("Cell count: " + cellCount);
+                        this.currentPanel.console.cells.get(cellCount-1).close();
                     });
                 })
             })
         }
         else {
-            //Temp cmd for testing
-            var injectCmd = "import cdms2\nimport vcs\ndata = cdms2.open(\'" +
-            this.context.session.path + "\')\nclt = data('clt')";
-            this.currentPanel.console.inject(injectCmd);
+            // Just inject code, console exists
+            console.log('Executing command console: inject');
+            this.currentPanel.session.ready.then(() => {
 
-            //Command to pull variables
-            this.currentPanel.console.inject(getVarCmd);
-            //this.currentPanel.console.clear();
+                //Command to put variables
+                this.currentPanel.console.inject(GET_VARS_CMD).then((consolePanel)=>{
+
+                    //Once injection is done read output as string
+                    var consoleOutputStr: string = document.getElementsByClassName("jp-OutputArea-output")[0].getElementsByTagName("pre")[0].innerHTML;
+                    console.log(consoleOutputStr);
+                    var outputObj = this.outputStrToDict(consoleOutputStr);
+
+                    //Update props
+                    this.props.variables = outputObj["variables"];
+                    this.props.templates = outputObj["templates"];
+                    this.props.graphicsMethods = outputObj["graphicsMethods"];
+
+                    //Rerender the LeftSideBar
+                    this.reRender();
+
+                    //Calc the number of cells in console and remove last cell once props are updated
+                    var cellCount = this.currentPanel.console.cells.length;
+                    console.log("Cell count: " + cellCount);
+                    this.currentPanel.console.cells.get(cellCount-1).close();
+                });
+            });
         }
     }
 }
