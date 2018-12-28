50905717ea42b3876baed7a3e1c61ed38d5c9e3a
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-12-28 12:14:16 -0800

    updated file locator

--- tests/lib/MainPage.py
+++ tests/lib/MainPage.py
@@ -54,10 +54,14 @@ class MainPage(BasePage):
 
                 print("... double clicking on file name")
                 #file_locator = "//li[@class='jp-DirListing-item'][@title='{f}']".format(f=fname)
-                locator1 = "//li[@class='jp-DirListing-item'][@title='{f}']".format(f=fname)
-                locator2 = "//span[@class='jp-DirListing-itemText']"
-                file_locator = "{l1}{l2}".format(l1=locator1, l2=locator2)
-                file_element = self.driver.find_element_by_xpath(file_locator)
+
+                #locator1 = "//li[@class='jp-DirListing-item'][@title='{f}']".format(f=fname)
+                #locator2 = "//span[@class='jp-DirListing-itemText']"
+                #file_locator = "{l1}{l2}".format(l1=locator1, l2=locator2)
+                #file_element = self.driver.find_element_by_xpath(file_locator)
+                locator1 = "//li[@class='jp-DirListing-item'][@title='{f}']//span".format(f=fname)
+                file_element = self.driver.find_elements_by_xpath(locator1)[1]
+
                 print("...found the element...")
                 actionChains = ActionChains(self.driver)
                 actionChains.move_to_element(file_element)
