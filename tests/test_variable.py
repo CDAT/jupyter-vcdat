735b294024b8393100895c9cb34b25bf8d4366b7
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-01-04 16:16:11 -0800

    rename testcase to test_plotvariable1

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -7,9 +7,6 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 lib_dir = os.path.join(this_dir, 'lib')
 sys.path.append(lib_dir)
 
-#print("xxx xxx lib_dir: {d}".format(d=lib_dir))
-
-# from BaseTestCase import BaseTestCase
 from BaseTestCase import BaseTestCase
 from BasePage import InvalidPageException
 from MainPage import MainPage
@@ -18,16 +15,12 @@ from JupyterUtils import JupyterUtils
 
 class BrowserTest(BaseTestCase):
 
-    def test_load_variable1(self):
+    def test_plot_variable1(self):
         print("xxx test_vcdat_jupyter_lab xxx")
         utils = JupyterUtils()
         server = utils.get_server()
 
-        ##ws = "http://localhost:8888/?token=4a25c7eb4ac7042da403eecbac6691a51132543b95b4e613"
-        #ws = "http://www.google.com"
-        #ws = "http://localhost:8888"
         main_page = MainPage(self.driver, server)
-
         main_page.load_file("clt.nc")
 
         # validate that we get a notebook
