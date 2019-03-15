b11f931f16b363461bee7baa494ac219bc97733d
Charles Doutriaux <doutriaux1@llnl.gov>
2019-03-15 09:48:02 -0700

    looks like a good set of dockerfiles

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,11 @@
-python -m pip install lazy_import
+#pip install lazy_import
 # Install sidecar
-python -m pip install sidecar
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install @jupyter-widgets/jupyterlab-sidecar
-python setup.py
-npm install
-npm run build
-jupyter lab build
-jupyter-labextension install .
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
+#jupyter labextension install @jupyter-widgets/jupyterlab-sidecar
+$PYTHON setup.py install
+$PYTHON  -m pip install --no-deps --ignore-installed sidecar==0.2.0
+#npm install
+#npm run build
+#jupyter lab build
+#jupyter-labextension install .
 
