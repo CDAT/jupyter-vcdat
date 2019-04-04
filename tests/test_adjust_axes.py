9ef16a9e33787333eb1ea9992436712a7132833e
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-04-04 15:04:57 -0700

    start each testcase with creating a new notebook, and rename the notebook, and delete the pynb at the end of test case

--- tests/test_adjust_axes.py
+++ tests/test_adjust_axes.py
@@ -16,9 +16,29 @@ class TestAdjustAxes(BaseTestCase):
 
     def test_adjust_axes_1(self):
         '''
-        load 'clt.nc', load 'u' variable, load 'v' variable, adjust sliders, load, and plot.
+        load 'clt.nc', load 'u' variable, adjust slider, load, and plot.
         '''
         print("\n\n...test_adjust_axes_1...")
+
+        left_side_bar = self.load_data_file("clt.nc")
+
+        load_variable_pop_up = LoadVariablePopUp(self.driver)
+        load_variable_pop_up.click_on_var('u')
+        load_variable_pop_up.click_on_var_axes('u')
+
+        # adjust the min slider by 20 percent and max slider by 20 percent.
+        load_variable_pop_up.adjust_var_axes_slider('u', 'latitude1', 20, -20)
+
+        load_variable_pop_up.load()
+        left_side_bar.click_on_plot()
+        canvas = Canvas(self.driver)
+        canvas.check_plot()
+
+    def test_adjust_axes_2(self):
+        '''
+        load 'clt.nc', load 'u' variable, load 'v' variable, adjust sliders, load, and plot.
+        '''
+        print("\n\n...test_adjust_axes_2...")
 
         left_side_bar = self.load_data_file("clt.nc")
 
--- tests/test_variable.py
+++ tests/test_variable.py
@@ -17,14 +17,12 @@ class TestLoadVariable(BaseTestCase):
         load 'clt.nc', load 'clt' variable, load and plot.
         '''
         print("\n\n...test_plot_variable_1...")
+        print("xxx xxx xxx {t}".format(t=self._testMethodName))
 
         left_side_bar = self.load_data_file("clt.nc")
 
         load_variable_pop_up = LoadVariablePopUp(self.driver)
-        try:
-            load_variable_pop_up.click_on_var('clt')
-        except NoSuchElementException:
-            print("XXX XXX XXX got exception XXX")
+        load_variable_pop_up.click_on_var('clt')
 
         load_variable_pop_up.load()
 
@@ -50,26 +48,6 @@ class TestLoadVariable(BaseTestCase):
 
         left_side_bar.click_on_plot()
 
-        canvas = Canvas(self.driver)
-        canvas.check_plot()
-
-    def test_plot_variable_3(self):
-        '''
-        load 'clt.nc', load 'u' variable, adjust slider, load, and plot.
-        '''
-        print("\n\n...test_plot_variable_3...")
-
-        left_side_bar = self.load_data_file("clt.nc")
-
-        load_variable_pop_up = LoadVariablePopUp(self.driver)
-        load_variable_pop_up.click_on_var('u')
-        load_variable_pop_up.click_on_var_axes('u')
-
-        # adjust the min slider by 20 percent and max slider by 20 percent.
-        load_variable_pop_up.adjust_var_axes_slider('u', 'latitude1', 20, -20)
-
-        load_variable_pop_up.load()
-        left_side_bar.click_on_plot()
         canvas = Canvas(self.driver)
         canvas.check_plot()
 
