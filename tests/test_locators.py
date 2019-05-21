77c1130a6f64934730d2d8eed3c506b4779f2859
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-05-20 22:05:13 -0700

    just rerun test_file_browser_locators()

--- tests/test_locators.py
+++ tests/test_locators.py
@@ -115,3 +115,4 @@ if __name__ == '__main__':
     unittest.main(verbosity=2)
 
 # nosetests -s tests/test_locators.py:TestLocators.test_new_notebook
+# nosetests -s tests/test_locators.py:TestLocators.test_file_browser_locators
