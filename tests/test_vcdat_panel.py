11194b15b2316ab375e47a8b318f609800afbe47
Carlos Downie <42552189+downiec@users.noreply.github.com>
2020-03-10 17:41:23 -0700

    Added more error data upon element not found.

--- tests/test_vcdat_panel.py
+++ tests/test_vcdat_panel.py
@@ -52,11 +52,11 @@ class TestVcdatPanel(BaseTestCase):
 
         # Check graphics group options available (random)
         vcdat_panel.plot_type_group("boxfill").scroll_click()
-        vcdat_panel.plot_type_group("scatter").scroll_click().sleep(1)
+        vcdat_panel.plot_type_group("scatter").scroll_click()
         vcdat_panel.plot_type_group("xvsy").scroll_click()
-        vcdat_panel.plot_type_group("1d").scroll_click().sleep(1)
+        vcdat_panel.plot_type_group("1d").scroll_click()
         vcdat_panel.plot_type_group("3d_dual_scalar").scroll_click()
-        vcdat_panel.plot_type_group("3d_vector").scroll_click().sleep(1)
+        vcdat_panel.plot_type_group("3d_vector").scroll_click()
 
         # Test close button
         vcdat_panel.plot_type_group("isoline").scroll_click()
