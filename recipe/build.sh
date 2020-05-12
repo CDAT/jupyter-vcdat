7c86da37d1e7206f2f4104f9b03ab8ee795942d1
Carlos Downie <42552189+downiec@users.noreply.github.com>
2020-05-11 18:10:06 -0700

    added source command to see if conda command will run. Updated template

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 conda list
-pip install https://files.pythonhosted.org/packages/e9/29/1a8ba5daffc63f883f23fc2012a5beeea320fe57e8c681ab4b11de30da7a/sidecar-0.3.0-py2.py3-none-any.whl
+pip install sidecar
 jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
 jupyter labextension install @jupyter-widgets/jupyterlab-sidecar --no-build
 jupyter labextension install jupyterlab-tutorial-extension --no-build
