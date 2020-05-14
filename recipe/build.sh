c0845348523aa78ad849c1127dfe7849d191a3c8
Carlos Downie <42552189+downiec@users.noreply.github.com>
2020-05-14 16:04:51 -0700

    Fixed deploy script and updated build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 conda list
-https://files.pythonhosted.org/packages/55/a4/6c81cc08e5a307e692659fdc3a9a1a031d173a4c79d0539cbd20e357fa75/sidecar-0.4.0-py2.py3-none-any.whl
+pip install https://files.pythonhosted.org/packages/55/a4/6c81cc08e5a307e692659fdc3a9a1a031d173a4c79d0539cbd20e357fa75/sidecar-0.4.0-py2.py3-none-any.whl
 jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
 jupyter labextension install @jupyter-widgets/jupyterlab-sidecar --no-build
 jupyter labextension install jupyterlab-tutorial-extension --no-build
--- scripts/deploy_update.py
+++ scripts/deploy_update.py
@@ -104,7 +104,7 @@ def update_template(template_in, file_out, data):
 def create_build_script(template_in, script_out):
 
     # Generate pip install commands
-    pip = SIDECAR_PIP
+    pip = f"pip install {SIDECAR_PIP}"
 
     # Generate extension install commands
     EXTS = BASE_EXTENSIONS + EXTRA_EXTENSIONS
